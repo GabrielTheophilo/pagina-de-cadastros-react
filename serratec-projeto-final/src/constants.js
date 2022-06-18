83046984edd66d1da91646142fe1b9d41593122e
isabeels <101607831+isabeels@users.noreply.github.com>
2022-06-18 00:02:37 -0300

    Projeto desenvolvido pelo grupo 5

---
+++ serratec-projeto-final/src/constants.js
@@ -0,0 +1,2 @@
+export const API_URL_ALUNOS = "http://localhost:3002/alunos";
+export const API_URL_MATERIA = "http://localhost:3002/materias";
---
+++ serratec-projeto-final/src/context/index.js
@@ -0,0 +1,51 @@
+import { createContext, useState } from "react";
+
+export const TemaContext = createContext();
+export const TemaProvider = ({ children }) => {
+  const [temaSelecionado, setTemaSelecionado] = useState(
+    "claro" // informa o valor inicial do estado
+  );
+
+  return (
+    <TemaContext.Provider
+      value={{
+        temaSelecionado,
+        setTemaSelecionado,
+      }}
+    >
+      {children}
+    </TemaContext.Provider>
+  );
+};
+
+export const UsuarioContext = createContext();
+export const UsuarioProvider = ({ children }) => {
+  const [usuario, setUsuario] = useState(localStorage.getItem("usuarioLogado"));
+
+  return (
+    <UsuarioContext.Provider
+      value={{
+        usuario,
+        setUsuario,
+      }}
+    >
+      {children}
+    </UsuarioContext.Provider>
+  );
+};
+
+export const AlunosContext = createContext();
+export const AlunosProvider = ({ children }) => {
+  const [alunos, setAlunos] = useState([]);
+
+  return (
+    <AlunosContext.Provider
+      value={{
+        alunos,
+        setAlunos,
+      }}
+    >
+      {children}
+    </AlunosContext.Provider>
+  );
+};
